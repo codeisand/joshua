61fe279b76f2b3352c5ac4e7c611983794ea10c4
codeisand <codeisand@gmail.com>
2022-11-25 12:08:08 +0530

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'codeisand.joshua',
+  appName: 'joshua',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
